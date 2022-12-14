3cdafb0648ad216aeea9073de95aa962ac978f06
victor dianno <victor.gdianno@live.com>
2022-12-14 18:24:42 -0300

    Fist Commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found!' });
+  }
+
+  request.user = user;
+ 
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user;
+
+  if (user.pro === false) {
+    if (user.todos.length >= 10) {
+      return response.status(403).json({ error: 'User without availability!' });
+    }
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found!' });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id not a uuid!' });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
+ 
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username not found!' });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -56,7 +104,7 @@ app.get('/users/:id', findUserById, (request, response) => {
 app.patch('/users/:id/pro', findUserById, (request, response) => {
   const { user } = request;
 
-  if (user.pro) {
+  if (user.pro === true) {
     return response.status(400).json({ error: 'Pro plan is already activated.' });
   }
 
